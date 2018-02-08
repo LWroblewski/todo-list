c8263770fb141d04928076080d8010e3b19bac2e
Wroblewski Laurent [i-BP] <Laurent.WROBLEWSKI_ssii@i-bp.fr>
2018-02-08 13:54:16 +0100

    fix(*): remove useless subscription

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,19 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TodoService} from '../core/components/services/todo.service';
 import {Todo} from '../core/components/model/todo.model';
-import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent implements OnInit {
 
-  todoSubscription$: Subscription;
   todos: Todo[];
 
   constructor(private todoService: TodoService) { }
 
   ngOnInit() {
-    this.todoSubscription$ = this.todoService.getTodos()
-      .subscribe(todos => this.todos = todos);
+    this.todoService.getTodos().subscribe(todos => this.todos = todos);
   }
 
   deleteTodo(pTodo: Todo) {
@@ -28,9 +25,5 @@ export class HomeComponent implements OnInit, OnDestroy {
       title: 'La flemme de faire des inputs',
       message: 'Commentaire fixe'
     });
-  }
-
-  ngOnDestroy(): void {
-    this.todoSubscription$.unsubscribe();
   }
 }
